3d9c43442d91c4ac13314e59fc841e9f53eb93ba
Andrei <andrei.stefan29@gmail.com>
2020-02-02 12:04:13 +0200

    First commit

---
+++ src/WebMapView/WebMapView.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { loadModules } from 'esri-loader';
+import './WebMapView.css';
+
+class WebMapView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.mapRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // lazy load the required ArcGIS API for JavaScript modules and CSS
+    loadModules(['esri/Map', 'esri/views/MapView'], { css: true })
+    .then(([ArcGISMap, MapView]) => {
+      const map = new ArcGISMap({
+        basemap: 'topo-vector'
+      });
+
+      this.view = new MapView({
+        container: this.mapRef.current,
+        map: map,
+        center: [-118, 34],
+        zoom: 8
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.view) {
+      // destroy the map view
+      this.view.container = null;
+    }
+  }
+
+  render() {
+    return (
+      <div className="webmap" ref={this.mapRef} />
+    );
+  }
+}
+
+export default WebMapView;
